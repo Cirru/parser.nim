ca1883fe0c66ef475e76857d835602dc76c25d23
jiyinyiyong <jiyinyiyong@gmail.com>
2019-09-03 00:58:26 +0800

    pretty print errors based on line numbers

--- tests/testLineNumbers.nim
+++ tests/testLineNumbers.nim
@@ -3,11 +3,33 @@ import unittest
 import cirruParser/types
 import cirruParser/lexer
 import cirruParser/helpers
+import cirruParser
 
 test "Line number results":
   echo lexCode("a\n  b")
+
+  echo()
+
+  let code = "a \""
   try:
-    echo lexCode("a \"")
+    echo lexCode(code)
   except CirruParseError as e:
-    echo e.msg
-    echo e.line, ":", e.column
+    echo formatParserFailure(code, e.msg, e.line, e.column)
+
+  echo()
+
+  let code2 = "a\n b"
+  try:
+    echo lexCode(code2)
+  except CirruParseError as e:
+    echo formatParserFailure(code2, e.msg, e.line, e.column)
+
+  echo()
+
+  let code3 = "a b c d e ) f"
+  try:
+    echo parseCirru(code3)
+  except CirruParseError as e:
+    echo formatParserFailure(code3, e.msg, e.line, e.column)
+
+  echo()
