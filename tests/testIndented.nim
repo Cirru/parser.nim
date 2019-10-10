efe0fffe01365701b3967ea285ccb84397f51760
jiyinyiyong <jiyinyiyong@gmail.com>
2019-10-11 02:27:27 +0800

    adding two more cases of indentations

--- tests/testIndented.nim
+++ tests/testIndented.nim
@@ -10,3 +10,13 @@ test "Read parentheses with indent":
   let dataCode = readFile("tests/data/paren-indent.json")
   let sourceCode = readFile("tests/cirru/paren-indent.cirru")
   check (toCirru(parseJson(dataCode)) == parseCirru(sourceCode))
+
+test "Read indent twice":
+  let dataCode = readFile("tests/data/indent-twice.json")
+  let sourceCode = readFile("tests/cirru/indent-twice.cirru")
+  check (toCirru(parseJson(dataCode)) == parseCirru(sourceCode))
+
+test "Read let indent":
+  let dataCode = readFile("tests/data/let.json")
+  let sourceCode = readFile("tests/cirru/let.cirru")
+  check (toCirru(parseJson(dataCode)) == parseCirru(sourceCode))
