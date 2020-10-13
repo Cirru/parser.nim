7c8184a01ad15456bbe4a7d5becde36468809b76
jiyinyiyong <jiyinyiyong@gmail.com>
2020-10-13 11:22:47 +0800

    echo perf time

--- tests/parse_cost.nim
+++ tests/parse_cost.nim
@@ -1,11 +1,17 @@
 
 import nimprof
 import cirru_parser
+import times
 
 # let file = "/Users/chen/repo/cirru/calcit-runner.nim/example/compact.cirru"
-let file = "/Users/chen/repo/cirru/calcit-editor/calcit.cirru"
+# let file = "/Users/chen/repo/cirru/calcit-editor/calcit.cirru"
 # let file = "/Users/chen/repo/cirru/bisection-key/calcit.cirru"
+let file = "/Users/chen/repo/mvc-works/calcit-workflow/calcit.cirru"
 
+let t1 = now()
 echo "start"
 discard parseCirru(readFile(file))
 echo "finish"
+
+let t2 = now()
+echo "Cost: ", t2 - t1
